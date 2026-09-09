0c930bb826ec325bf3e89339506f812a8b36bef2
abdullah amr <abdullahamrmohamedesmat@@gmail.com>
2026-09-09 20:00:09 +0300

    1st answer

--- Assig.oop01/Program.cs
+++ Assig.oop01/Program.cs
@@ -6,7 +6,37 @@ namespace Assig.oop01
     {
         static void Main(string[] args)
         {
-          
+
+            #region Question 1
+            //1st answer
+            DeliveryAdddress deliveryAdddress = new DeliveryAdddress() { city = "cairo", street = "abbas" };
+            DeliveryAdddress deliveryAdddress1;
+
+            deliveryAdddress1 = deliveryAdddress;
+            Console.WriteLine(deliveryAdddress);
+
+            Console.WriteLine(deliveryAdddress1);
+            Console.WriteLine();
+
+
+            deliveryAdddress1.city = "Giza";
+            deliveryAdddress1.street = "alharam";
+
+            Console.WriteLine(deliveryAdddress);
+            Console.WriteLine(deliveryAdddress1); //struct is vlaue type,Dont't affected
+            Console.WriteLine();
+
+            Customer customer = new Customer();
+            Customer customer2;
+            customer2 = customer;
+            Console.WriteLine(customer);
+            Console.WriteLine(customer2);
+            Console.WriteLine();
+
+            customer2.Name = "Ahmed";
+            Console.WriteLine(customer);
+            Console.WriteLine(customer2);//class is Ref Type ,affected 
+            #endregion
 
         }
     }
