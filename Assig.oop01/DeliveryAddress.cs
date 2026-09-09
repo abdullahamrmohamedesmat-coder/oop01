c03d95cf050cb1371c27e320893299a5813468f4
abdullah amr <abdullahamrmohamedesmat@@gmail.com>
2026-09-09 22:11:59 +0300

    3st answer

---
+++ Assig.oop01/DeliveryAddress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assig.oop01
+{
+    internal struct DeliveryAddress
+    {
+        public string city { get; set; }
+        public string street { get; set; }
+        public int buildingNumber { get; set; }
+        public DeliveryAddress(string City,string Street,int BuildingNumber)
+        {
+            city = City; 
+             street=Street;
+            buildingNumber = BuildingNumber;
+        }
+      
+
+        public string GetAddress()
+        {
+            return $"{buildingNumber}, { city}, {street}";
+        }
+    }
+}
--- Assig.oop01/Program.cs
+++ Assig.oop01/Program.cs
@@ -9,44 +9,59 @@ namespace Assig.oop01
 
             #region Question 1
             //1st answer
-            DeliveryAdddress deliveryAdddress = new DeliveryAdddress() { city = "cairo", street = "abbas" };
-            DeliveryAdddress deliveryAdddress1;
+            //DeliveryAdddress deliveryAdddress = new DeliveryAdddress() { city = "cairo", street = "abbas" };
+            //DeliveryAdddress deliveryAdddress1;
 
-            deliveryAdddress1 = deliveryAdddress;
-            Console.WriteLine(deliveryAdddress);
+            //deliveryAdddress1 = deliveryAdddress;
+            //Console.WriteLine(deliveryAdddress);
 
-            Console.WriteLine(deliveryAdddress1);
-            Console.WriteLine();
+            //Console.WriteLine(deliveryAdddress1);
+            //Console.WriteLine();
 
 
-            deliveryAdddress1.city = "Giza";
-            deliveryAdddress1.street = "alharam";
+            //deliveryAdddress1.city = "Giza";
+            //deliveryAdddress1.street = "alharam";
 
-            Console.WriteLine(deliveryAdddress);
-            Console.WriteLine(deliveryAdddress1); //struct is vlaue type,Dont't affected
-            Console.WriteLine();
+            //Console.WriteLine(deliveryAdddress);
+            //Console.WriteLine(deliveryAdddress1); //struct is vlaue type,Dont't affected
+            //Console.WriteLine();
 
-            Customer customer = new Customer();
-            Customer customer2;
-            customer2 = customer;
-            Console.WriteLine(customer);
-            Console.WriteLine(customer2);
-            Console.WriteLine();
+            //Customer customer = new Customer();
+            //Customer customer2;
+            //customer2 = customer;
+            //Console.WriteLine(customer);
+            //Console.WriteLine(customer2);
+            //Console.WriteLine();
 
-            customer2.Name = "Ahmed";
-            Console.WriteLine(customer);
-            Console.WriteLine(customer2);//class is Ref Type ,affected 
+            //customer2.Name = "Ahmed";
+            //Console.WriteLine(customer);
+            //Console.WriteLine(customer2);//class is Ref Type ,affected 
             #endregion
 
             #region Question 2
-            //2st answer
-            Shipment shipment = new Shipment();
-            shipment.Description = "test";
-            Console.WriteLine(shipment.Description);
+            ////2st answer
+            //Shipment shipment = new Shipment();
+            //shipment.Description = "test";
+            //Console.WriteLine(shipment.Description);
 
-            
+
             #endregion
 
+            #region Question 3
+            //3st answer
+            DeliveryAddress deliveryAdddress = new DeliveryAddress("Cairo", "Abbass", 33);
+            DeliveryAddress deliveryAddress01;
+            deliveryAddress01 = deliveryAdddress;
+            Console.WriteLine(deliveryAdddress.GetAddress());
+            Console.WriteLine(deliveryAddress01.GetAddress());
+            Console.WriteLine();
+            deliveryAddress01.city = "Giza";
+            deliveryAddress01.street = "Alharam";
+            deliveryAddress01.buildingNumber = 23;
+            Console.WriteLine(deliveryAdddress.GetAddress());
+            Console.WriteLine(deliveryAddress01.GetAddress());
+            Console.WriteLine(); 
+            #endregion
         }
     }
 }
